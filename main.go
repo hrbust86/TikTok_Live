0c2849f8c9644199f56bdc808620f81b45b3743b
jiangwei <jiangwei@zuler.io>
2025-09-01 18:57:03 +0800

    去掉web服务器的代码

--- main.go
+++ main.go
@@ -7,16 +7,12 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"strings"
-	"sync"
-	"time"
 
 	"google.golang.org/protobuf/reflect/protoreflect"
 
 	tiktok_hack "Sunny/tiktok_hack/generated"
 
-	"github.com/gorilla/websocket"
 	"github.com/qtgolang/SunnyNet/SunnyNet"
 	"github.com/qtgolang/SunnyNet/src/public"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -24,7 +20,8 @@ import (
 )
 
 var Sunny = SunnyNet.NewSunny()
-var agentlist sync.Map // 使用 sync.Map 替代 map[string]*agent
+
+//var agentlist sync.Map // 使用 sync.Map 替代 map[string]*agent
 
 // agent 结构体用于存储 WebSocket 连接及其相关信息
 func main() {
@@ -42,116 +39,13 @@ func main() {
 		log.Fatalf(st.Error.Error())
 	}
 
-	// 设置 WebSocket 升级器
-	upgrader := websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool {
-			return true // 允许所有 CORS 请求
-		},
-	}
-
-	// 处理 WebSocket 请求
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		handleWebSocket(upgrader, w, r)
-	})
-
 	fmt.Println("浏览器代理设置为:127.0.0.1:23809")
 	fmt.Println("上游代理地址为:127.0.0.1:21586")
-	fmt.Println("WebSocket 服务地址为:ws://127.0.0.1:18080/ws")
+	//fmt.Println("WebSocket 服务地址为:ws://127.0.0.1:18080/ws")
 	fmt.Println("正在运行....")
-
-	// 启动 HTTP 服务器（用于 WebSocket）
-	go func() {
-		err := http.ListenAndServe(":18080", nil)
-		if err != nil {
-			log.Fatalf("ListenAndServe: %v", err)
-		}
-	}()
-
-	log.Println("WebSocket 服务启动成功.ws端口为18080")
 
 	// 避免程序退出
 	select {}
-}
-
-// handleWebSocket 处理 WebSocket 连接
-func handleWebSocket(upgrader websocket.Upgrader, w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("WebSocket 升级失败:", err)
-		return
-	}
-	defer conn.Close()
-
-	key := r.Header.Get("Sec-WebSocket-Key")
-	if key == "" {
-		log.Println("缺少 Sec-WebSocket-Key 头")
-		return
-	}
-
-	agent := &agent{
-		queue: make(chan []byte, 2),
-		stop:  make(chan struct{}),
-		conn:  conn,
-	}
-	agentlist.Store(key, agent)
-	go handleClient(agent)
-
-	log.Println("当前连接数", getAgentCount())
-
-	// 设置读取超时时间
-	err = agent.conn.SetReadDeadline(time.Now().Add(30 * time.Second))
-	if err != nil {
-		log.Println("设置读取超时失败:", err)
-		return
-	}
-
-	defer func() {
-		log.Println(key, "断开连接")
-		close(agent.stop)
-		agentlist.Delete(key)
-	}()
-
-	for {
-		mt, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("读取消息失败:", err)
-			break
-		}
-		log.Printf("收到消息: %s", message)
-		if string(message) == "ping" {
-			if err := conn.WriteMessage(mt, []byte("pong")); err != nil {
-				log.Println("发送消息失败:", err)
-				break
-			} else {
-				_ = agent.conn.SetReadDeadline(time.Now().Add(30 * time.Second))
-			}
-		}
-	}
-}
-
-// handleClient 处理客户端消息
-func handleClient(a *agent) {
-	for {
-		select {
-		case <-a.stop:
-			return
-		case b := <-a.queue:
-			err := a.conn.WriteMessage(websocket.TextMessage, b)
-			if err != nil {
-				log.Println("发送消息失败:", err)
-			}
-		}
-	}
-}
-
-// getAgentCount 获取当前连接数
-func getAgentCount() int {
-	count := 0
-	agentlist.Range(func(_, _ interface{}) bool {
-		count++
-		return true
-	})
-	return count
 }
 
 // HttpCallback HTTP 回调函数
@@ -226,12 +120,6 @@ func WSCallback(Conn SunnyNet.ConnWebSocket) {
 				processChatMessage(marshal)
 			}
 
-			// 遍历 agentlist，将消息发送到每个连接
-			agentlist.Range(func(_, value interface{}) bool {
-				agent := value.(*agent)
-				agent.queue <- marshal
-				return true
-			})
 		}
 
 	}
